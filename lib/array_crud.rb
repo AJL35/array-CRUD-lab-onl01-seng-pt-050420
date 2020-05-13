ece2bd359fa7a2c9e88e3ecc5e7e85a3c4393f39
andre lawrence <andrelawrence35@gmail.com>
2020-05-13 13:30:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ add_element_to_start_of_array = ("array", "element")
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array= ("array" "element")
+  add_element_to_start_of_array = ("array", "element")
 add_element_to_start_of_array.unsift("wow")
   
 end
