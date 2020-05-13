e4a9b934c0cd79cfc531ac638010c70d587b0c15
andre lawrence <andrelawrence35@gmail.com>
2020-05-13 13:22:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,8 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   puppies = ["bulldog", "terrier", "poodle", "pit bull"]
-   puppies.push ("arrays!")
+
+   add_element_to_start_of_array.push ("arrays!")
 
 
 end
